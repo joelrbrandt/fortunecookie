6beff2b58960ae576b2501d8e532e48732c1255e
joelrbrandt <joelrbrandt@cad8b1ca-0341-0410-b41d-8953ea0747f7>
2008-02-06 18:27:27 +0000

    working on stupid slider. still needs to have updateTime not change the slider when it's being dragged. mermermer

--- js/fortunecookie.js
+++ js/fortunecookie.js
@@ -69,27 +69,93 @@ function playMovie() {
 }
 
 function gotoSpot(timeInSec) {
-    getFlashMovie("myScrubber").gotoTime(timeInSec);
+    if(playerWindow && playerWindow.mdLoaded) {
+	getFlashMovie("myScrubber").gotoTime(timeInSec);
+    }
 }
 
 function displaySpecs() {
     if(playerWindow && playerWindow.mdLoaded) {
-	var duration = getFlashMovie("myScrubber").getDuration();
-	var current = getFlashMovie("myScrubber").getMovieTime();
+	var current = getCurrentPositionInSeconds();
+	var duration = getMovieDurationInSeconds();
         document.getElementById("specs").innerHTML = "Length: " + duration + "<br>Current: " + current;
     }
 }
 
+function updateTime() {
+    var current = getCurrentPositionInSeconds();
+    var duration = getMovieDurationInSeconds();
+    if (!current) current = 0;
+    if (!duration) duration = 1;
+    position = current * 800 / duration;
+    $('#time').text('' + formatTime(current) + ' (' + parseInt(current * 100 / duration) + '%)');
+    setScrubberValue(parseInt(position));
+}
 
-function switchVideo() {
+function getCurrentPositionInSeconds() {
+    if(playerWindow && playerWindow.mdLoaded) {
+	return getFlashMovie("myScrubber").getMovieTime();
+    } else {
+	return null;
+    }
+}
+
+function getMovieDurationInSeconds() {
+    if(playerWindow && playerWindow.mdLoaded) {
+	return getFlashMovie("myScrubber").getDuration();
+    } else {
+	return null;
+    }
+}
+
+function formatTime(time) {
+    seconds = time % 60;
+    minutes = parseInt(time / 60);
+    if (seconds < 10) {
+	return '' + minutes + ':0' + seconds;
+    } else {
+	return '' + minutes + ':' + seconds;
+    }
+}
+
+function switchVideoButton() {
     var targetElement1 = document.getElementById("domain");
     var targetElement2 = document.getElementById("vidName");
     var dom = targetElement1.value;
     var vid = targetElement2.value;
-    playerWindow.metadataUnloaded();
-    getFlashMovie("myScrubber").setDomain(dom);
-    getFlashMovie("myScrubber").setFilename(vid);
+    switchVideo(dom, vid);
+}
 
+function switchVideo(domain, filename) {
+    playerWindow.metadataUnloaded();
+    getFlashMovie("myScrubber").setDomain(domain);
+    getFlashMovie("myScrubber").setFilename(filename);
+}
+
+function onScrubberChange(value, slider) {
+    // calculate the time in seconds to scrub to
+    var duration = getMovieDurationInSeconds();
+    if (!duration) duration = 0;
+    time = duration * value / 800;
+    gotoSpot(parseInt(time));
+    updateTime();
+}
+
+// initialize the slider when the page is ready
+$(function() {
+    initializeSlider($('#scrubber').get(0), $('#scrubberDisplay').get(0));
+    setSliderChangeCallback(onScrubberChange);
+    setScrubberValue(0);
+ });
+
+
+function setScrubberValue(v) {
+    $('#scrubberDisplay').get(0).value = v;
+    updateSliderToMatchDisplay($('#scrubber').get(0), $('#scrubberDisplay').get(0));
+}
+
+function getScrubberValue() {
+    return $('#scrubberDisplay').get(0).value;
 }
 
 function leadingZero(nr)
@@ -97,3 +163,7 @@ function leadingZero(nr)
     if (nr < 10) nr = "0" + nr;
     return nr;
 }
+
+$(function() {
+    setInterval(updateTime, 250);
+});
